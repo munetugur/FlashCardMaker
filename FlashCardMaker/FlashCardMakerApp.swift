3f8e81aeb05b854384cbe83d707b3bce8d94c514
munetugur <munemune0509@gmail.com>
2022-10-18 22:47:28 +0900

    Initial Commit

---
+++ FlashCardMaker/FlashCardMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardMakerApp.swift
+//  FlashCardMaker
+//
+//  Created by MNT on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
